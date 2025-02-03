7fe68d9ac762cebde433cea280bc68a51fe50d73
smash24 <asm.sadamhussain@gmail.com>
2025-02-03 11:21:38 +0530

    added middleware to solve cors

--- backend/app.js
+++ backend/app.js
@@ -27,10 +27,20 @@ app.use(cors({
     
  }))
 
+ app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'https://smash-blog.netlify.app');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  next();
+});
+
+
+
 app.use("/uploads" , express.static(path.join("public","uploads")))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended :true}))
 app.use(cookieParser())
+
 
 app.use(validateUser)
 
